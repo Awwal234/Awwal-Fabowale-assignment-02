93640a3889d23d2e64a1485bbfb435f1ea1ed885
Fabowale Muh-Awwal Ifedolapo <fabowalemuhawwal@gmail.com>
2022-07-03 11:29:43 +0000

    attempt -2

--- src/assignment.js
+++ src/assignment.js
@@ -20,7 +20,7 @@ function sumOfNumbers(arrayOfNumbers) {
     arrayOfNumbers.forEach((x) => {
         sum += x;
     });
-    return sum;
+    return n;
 }
 console.log(sum);
 assignment.sumOfNumbers = sumOfNumbers;
